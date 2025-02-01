348711d04b065152c32dd45a6bbb0a8ab703873d
Nikita Leev <nikita.leev@gmail.com>
2025-02-01 22:06:37 +0000

    Best Time to Buy and Sell Stock II

--- wystan/top-interview-questions-easy/src/arrays.rs
+++ wystan/top-interview-questions-easy/src/arrays.rs
@@ -13,3 +13,16 @@ pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
     pointer += 1;
     pointer as i32
 }
+
+pub fn max_profit(prices: Vec<i32>) -> i32 {
+    if prices.len() <= 1 {
+        return 0;
+    }
+    let mut profit = 0;
+    for i in 1..prices.len() {
+        if prices[i] > prices[i - 1] {
+            profit += prices[i] - prices[i - 1];
+        }
+    }
+    return profit;
+}
