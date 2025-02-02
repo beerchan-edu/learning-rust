b4206f6717e223c7fcf3cef8dd6efa43b1829791
Nikita Leev <nikita.leev@gmail.com>
2025-02-02 14:50:19 +0000

    Rotate Image

--- wystan/top-interview-questions-easy/src/arrays.rs
+++ wystan/top-interview-questions-easy/src/arrays.rs
@@ -131,3 +131,16 @@ pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
     }
     vec![0, 0]
 }
+
+pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+    let n = matrix.len();
+    for i in 0..n / 2 {
+        for j in i..n - i - 1 {
+            let temp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = temp;
+        }
+    }
+}
