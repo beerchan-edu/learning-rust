0c0b9579c76f7228d16a1610506d4d4e115574cf
Nikita Leev <nikita.leev@gmail.com>
2025-02-03 21:05:46 +0000

    Reverse integer

--- wystan/top-interview-questions-easy/src/strings.rs
+++ wystan/top-interview-questions-easy/src/strings.rs
@@ -1,11 +1,40 @@
 pub fn reverse_string(s: &mut Vec<char>) {
     let mut i: usize = 0;
     let mut j = s.len() - 1;
-    while (j > i) {
-        let mut tmp = s[i];
+    while j > i {
+        let tmp = s[i];
         s[i] = s[j];
         s[j] = tmp;
         i += 1;
         j -= 1;
     }
+}
+
+pub fn reverse(x: i32) -> i32 {
+    // 123 -> 3 -> 32 -> 321
+    if x == i32::MIN {
+        return 0;
+    }
+    let mut result: i32 = 0;
+    let mut reminder: i32 = x.abs();
+    while reminder > 0 {
+        let digit = reminder % 10;
+        reminder = reminder / 10;
+        match result.checked_mul(10) {
+            None => return 0,
+            Some(new_result) => match new_result.checked_add(digit) {
+                None => return 0,
+                Some(new_result) => {
+                    result = new_result;
+                }
+            },
+        }
+    }
+    if x < 0 {
+        match result.checked_mul(-1) {
+            None => return 0,
+            Some(result) => return result,
+        }
+    }
+    result
 }
