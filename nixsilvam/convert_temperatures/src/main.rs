f74e8b8962fd63fe4c58548a5062921a82289088
Nix Silvam <nixsilvam@icloud.com>
2025-02-06 23:13:42 +0200

    Chapter 3: solution for convert temperature task

---
+++ nixsilvam/convert_temperatures/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let celsius_result = fahrenheit_to_celsius(25.0);
+
+    println!("{celsius_result:.2}");
+
+    let fahrenheit_result = celsius_to_fahrenheit(50.0);
+
+    println!("{fahrenheit_result:.2}");
+
+}
+
+fn fahrenheit_to_celsius(degree: f64) -> f64 {
+    (degree - 32.0) * (5.0 / 9.0)
+}
+
+fn celsius_to_fahrenheit(degree: f64) -> f64 {
+    (degree * 9.0 / 5.0) + 32.0
+}
