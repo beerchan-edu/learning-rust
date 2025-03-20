20fd8c176968711c740918d4594f165bd84589e6
Nix Silvam <nixsilvam@icloud.com>
2025-03-20 22:32:59 +0200

    solution for Best profit from Sorted Array (not completed yet, i was ill)

--- nixsilvam/top_interview_questions_easy/src/best_profit.rs
+++ nixsilvam/top_interview_questions_easy/src/best_profit.rs
@@ -4,12 +4,12 @@ pub fn best_profit() {
 }
 
 fn finding_profit(prices: Vec<i32>) -> i32 {
-        let prices_len = len(prices);
-        let mut profit = 0;
-        for price in prices_len (
-            if prices[price] < prices[price + 1] {
-                profit = price;
-            }
-        )
-
+    let mut profit = 0;
+    for price in 0..(prices.len()) {
+        if prices[price] < prices[price + 1] {
+            profit += prices[price];
+            profit += prices[price + 1] - prices[price];
+        }
+    }
+    profit
 }
