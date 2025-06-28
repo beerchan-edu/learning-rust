f7ee0ea4ca37b974f5b3cf570ee52c56ea0c2fec
Nix Silvam <nixsilvam@icloud.com>
2025-06-28 16:05:26 +0300

    solution for rotate array

--- nixsilvam/top_interview_questions_easy/src/arrays/rotate_array.rs
+++ nixsilvam/top_interview_questions_easy/src/arrays/rotate_array.rs
@@ -1,13 +1,18 @@
 // https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
 
+
 pub fn solution_rotate() {
     let mut nums_1 = vec![1,2,3,4,5,6,7];
     let mut nums_2 = vec![-1,-100,3,99];
     println!("{:?}", (rotate_1(&mut nums_1, 3)));
     println!("{:?}", (rotate_1(&mut nums_2, 2)));
+    println!("{:?}", (rotate_2(&mut nums_1, 3)));
+    println!("{:?}", (rotate_2(&mut nums_2, 2)));
+    println!("{:?}", (rotate_3(&mut nums_1, 3)));
+    println!("{:?}", (rotate_3(&mut nums_2, 2)));
 }
 
-fn rotate_1(nums: &mut Vec<i32>, k: i32) -> &mut Vec<i32> { 
+fn rotate_1(nums: &mut Vec<i32>, k: u32) -> &mut Vec<i32> { 
     let mut step = 0;
     while step < k {
         let last_element = nums.pop();
@@ -17,5 +22,23 @@ fn rotate_1(nums: &mut Vec<i32>, k: i32) -> &mut Vec<i32> {
         }
         step += 1
     }
+    nums
+}
+
+fn rotate_2(nums: &mut Vec<i32>, k: u32) -> &mut Vec<i32> {
+    let steps: usize = k as usize;
+    nums.rotate_right(steps);
+    nums
+}
+
+fn rotate_3(nums: &mut Vec<i32>, k: u32) -> &mut Vec<i32> {
+    let len = nums.len();
+    let k = (k as usize) % len;
+    if k == 0 {return nums;}
+
+    nums[..len - k].reverse();
+    nums[len - k..].reverse();
+    nums.reverse();
+
     nums
 }
