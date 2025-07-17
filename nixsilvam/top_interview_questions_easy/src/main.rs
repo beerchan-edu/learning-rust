7916ab1393de42d88a9ff4d747ea072c2d7219ab
Nix Silvam <nixsilvam@icloud.com>
2025-07-17 22:29:26 +0300

    solution for reverse string task

--- nixsilvam/top_interview_questions_easy/src/main.rs
+++ nixsilvam/top_interview_questions_easy/src/main.rs
@@ -1,4 +1,5 @@
 mod arrays;
+mod strings;
 
 use std::{env};
 
@@ -13,16 +14,17 @@ fn main() {
 
     match args[1].as_str() {
         "727" => arrays::remove_duplicates_from_sorted_array::solution_remove_duplicates(),
-        "564" => arrays::best_time_to_buy_and_sell_stock_II::best_profit(),
+        "564" => arrays::best_time_to_buy_and_sell_stock_2::best_profit(),
         "646" => arrays::rotate_array::solution_rotate(),
         "578" => arrays::contains_duplicate::solution_contains_duplicates(),
         "549" => arrays::single_number::solution_single_numbers(),
-        "674" => arrays::intersection_of_two_arrays_II::solution_intersection(),
+        "674" => arrays::intersection_of_two_arrays_2::solution_intersection(),
         "559" => arrays::plus_one::solution_plus_one(),
         "567" => arrays::move_zeroes::solution_move_zeroes(),
         "546" => arrays::two_sum::solution_two_sums(),
         "769" => arrays::valid_sudoku::valid_sudoku(),
         "770" => arrays::rotate_image::Solution::rotate_image_solution(),
+        "879" => strings::reverse_string::Solution::reverse_string_solution(),
         _ => println!("Unknown task: {}", args[1]),
 
     }
---
+++ nixsilvam/top_interview_questions_easy/src/strings/reverse_string.rs
@@ -0,0 +1,25 @@
+// https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn reverse_string_solution() {
+        let mut s1: Vec<char> = vec!['h', 'e', 'l', 'l', 'o'];
+        let mut s2 = vec!['H', 'a', 'n', 'n', 'a', 'h'];
+
+        Solution::reverse_string(&mut s1);
+        Solution::reverse_string(&mut s2);
+
+        println!("{:?}", s1);
+        println!("{:?}", s1);
+    }
+
+    fn reverse_string(s: &mut Vec<char>) {
+        return s.reverse()
+    }
+}
+
+#[allow(dead_code)]
+fn main() {
+    Solution::reverse_string_solution();
+}
